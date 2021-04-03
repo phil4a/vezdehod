0cd8e5cbf3c512548b5d3a0d4987f184f0f9d5ef
phil <philjke@mail.ru>
2021-04-03 15:45:04 +0700

    adaptive+cursor

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -85,13 +85,13 @@ document.addEventListener('DOMContentLoaded', () => {
       let ind = swiperText.realIndex + 1,
          indRes = ind >= 10 ? ind : `0${ind}`
       gsap.to(curNum, .2, {
-         forse3D: true,
+         force3D: true,
          y: -10,
          opacity: 0,
          ease: Power2.easeOut,
          onComplete: function () {
             gsap.to(curNum, .1, {
-               forse3D: true,
+               force3D: true,
                y: 10
             })
             curNum.innerHTML = indRes
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       })
       gsap.to(curNum, .2, {
-         forse3D: true,
+         force3D: true,
          y: 0,
          opacity: 1,
          ease: Power2.easeOut,
@@ -108,5 +108,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
       })
    })
+
+   const body = document.querySelector('body'),
+      cursor = document.getElementById('cursor'),
+      links = document.getElementsByTagName('a')
+
+   let mouseX = 0, mouseY = 0, posX = 0, posY = 0
+
+   function mouseCoords(e) {
+      mouseX = e.pageX
+      mouseY = e.pageY
+   }
+   gsap.to({}, .01, {
+      repeat: -1,
+      onRepeat: () => {
+         posX += (mouseX - posX) / 6
+         posY += (mouseY - posY) / 6
+         gsap.set(cursor, {
+            css: {
+               left: posX,
+               top: posY
+            }
+         })
+      }
+   })
+
+   for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener('mouseover', () => {
+         cursor.classList.add('active')
+      })
+      links[i].addEventListener('mouseout', () => {
+         cursor.classList.remove('active')
+      })
+
+   }
+
+   body.addEventListener('mousemove', e => {
+      mouseCoords(e)
+      cursor.classList.remove('hidden')
+   })
+
+   body.addEventListener('mouseout', e => {
+      cursor.classList.add('hidden')
+   })
+
 
 })
